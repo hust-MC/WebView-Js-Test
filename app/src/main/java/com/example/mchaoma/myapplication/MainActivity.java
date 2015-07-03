22cfa4f93054ec5e8c68d2714038f207d7a32591
hust-MC <mc_zy@outlook.com>
2015-07-03 11:45:02 +0800

    first

---
+++ app/src/main/java/com/example/mchaoma/myapplication/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.mchaoma.myapplication;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends Activity
+{
+    private static final String LOG_TAG = "WebViewDemo";
+
+    private WebView mWebView;
+
+    private Handler mHandler = new Handler();
+
+    @Override
+    public void onCreate(Bundle icicle)
+    {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.web);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setSaveFormData(false);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSupportZoom(false);
+
+        mWebView.setWebChromeClient(new MyWebChromeClient());
+
+        mWebView.addJavascriptInterface(new DemoJavaScriptInterface(), "demo");
+
+        mWebView.loadUrl("file:///android_asset/addEventListener.html");
+    }
+
+    final class DemoJavaScriptInterface
+    {
+        DemoJavaScriptInterface()
+        {
+        }
+
+        /**
+         * This is not called on the UI thread. Post a runnable to invoke
+         * loadUrl on the UI thread.
+         */
+        @JavascriptInterface
+        public void clickOnAndroid()
+        {
+            mHandler.post(new Runnable()
+            {
+                public void run()
+                {
+                    Toast.makeText(MainActivity.this, "mcmcmc", Toast.LENGTH_SHORT).show();
+//                    mWebView.loadUrl("javascript:wave()");
+                }
+            });
+
+        }
+    }
+
+    /**
+     * Provides a hook for calling "alert" from javascript. Useful for
+     * debugging your javascript.
+     */
+    final class MyWebChromeClient extends WebChromeClient
+    {
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result)
+        {
+            Log.d(LOG_TAG, message);
+            result.confirm();
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if (keyCode == KeyEvent.KEYCODE_BACK && mWebView.canGoBack())
+        {
+            Log.d("MC", "back");
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
